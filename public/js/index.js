c659a287db87637ed985944e6875eee0fb1c472e
irislee <irislee.music@gmail.com>
2016-08-07 22:24:33 -0400

    added time difference

--- public/js/index.js
+++ public/js/index.js
@@ -11,22 +11,47 @@ request.onload = function() {
     
     var data = JSON.parse(request.responseText);
 
+    // function timeStamp() {
+    //   var now = new Date();
+    //   var date = [now.getFullYear(), now.getMonth() + 1, now.getDate()];
+    //   var time = [now.getHours(), now.getMinutes(), now.getSeconds() ];
+    //   return date.join("-") + " " + time.join(":");
+    // }
+
+    var currentDate = new Date();
+
+    function timeDiff(articleDate, currentDate) {
+      var publishDate = new Date(articleDate);
+      var diff = new Date(currentDate - publishDate);
+      var yearDiff   = diff.getUTCFullYear() - 1970;
+      var monthDiff  = diff.getUTCMonth();
+      var dayDiff    = diff.getUTCDate() - 1;
+      var hourDiff   = diff.getUTCHours();
+      var minuteDiff = diff.getUTCMinutes();
+      var secondDiff = diff.getUTCSeconds();
+
+      var difference = yearDiff + ' years ' + monthDiff + ' months ' + dayDiff + ' days ' + hourDiff + ' hours ' + minuteDiff + ' minutes and ' + secondDiff + ' seconds ago';
+
+      return difference;
+    }
+
     for (var i = 0; i < data.length; i++) {
       var table = document.getElementById('articles');
 
       var row = table.insertRow(-1);
 
-      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      // Insert new cells (<td> elements) at the "new" <tr> element:
       var title = row.insertCell(0);
       var name = row.insertCell(1);
       var words = row.insertCell(2);
-      var cell4 = row.insertCell(3);
+      var submitted = row.insertCell(3);
 
       // Add some text to the new cells:
       title.innerHTML = data[i].title;
-      name.innerHTML = data[i].profile.first_name + data[i].profile.last_name;
+      name.innerHTML = data[i].profile.first_name + ' ' + data[i].profile.last_name;
       words.innerHTML = data[i].words;
-      
+      submitted.innerHTML = timeDiff(data[i].publish_at, currentDate);
+
     };
 
     // document.getElementById("title").innerHTML = data[0].title;
